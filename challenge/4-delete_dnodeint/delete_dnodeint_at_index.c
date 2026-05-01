0d15d92a3bac99616cdc760513eb0e78d7b91b96
nawalsamer04 <nawalsamer04@gmail.com>
2026-05-01 23:11:26 +0000

    Final fix for double linked list

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -14,8 +14,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     if (index == 0)
     {
         *head = temp->next;
-        if (temp->next)
-            temp->next->prev = NULL;
+        if (*head)
+            (*head)->prev = NULL;
         free(temp);
         return (1);
     }
@@ -29,8 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     if (!temp)
         return (-1);
 
-    if (temp->prev)
-        temp->prev->next = temp->next;
+    temp->prev->next = temp->next;
 
     if (temp->next)
         temp->next->prev = temp->prev;
